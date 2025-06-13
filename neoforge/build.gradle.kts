3139d12be501c23dfd7c82fa1a978d417ce13fa9
Vladyslav Kingsley <sainthozier@gmail.com>
2025-06-13 07:34:48 +0300

    chore: update gradle and library versions

--- neoforge/build.gradle.kts
+++ neoforge/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 val modId: String by project
 
 neoForge {
-    version = libs.versions.neoforge
+    version = libs.versions.neoforge.get()
     // Automatically enable neoforge AccessTransformers if the file exists
     val at = project(":common").file("src/main/resources/META-INF/accesstransformer.cfg")
     if (at.exists()) {
